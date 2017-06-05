d3fa355f799d1e5187f11004071b93166d28f91f
Daniel Illanes <dany.gaara.ds@gmail.com>
2017-06-04 23:17:51 -0400

    Now it shows the results

--- features/Substract.feature
+++ features/Substract.feature
@@ -5,7 +5,7 @@ Feature: Substract two numbers
 
   Scenario: Substract two numbers
     Given I am on the WolframAlpha homepage
-    And I have entered 80 - 70 into the calculator
+    And I have entered 80 - 70 into the textbox
     When I click compute
     Then The result should be 10 on the screen
 
--- features/step_definitions/SubstractSteps.rb
+++ features/step_definitions/SubstractSteps.rb
@@ -1,14 +1,13 @@
 
 
-Given(/^I have entered (\d+)\-(\d+) into the textbox$/) do |arg1, arg2|
+Given(/^I have entered (\d+) - (\d+) into the textbox$/) do |arg1, arg2|
 fill_in 'query', :with => arg1+"-"+arg2
 end
 
 When(/^I click (\w+)$/) do |compute|
-   click(compute)
+   click_button('Submit')
 end
 
 Then /^The result should be (\d+) on the screen$/ do |n|
-  #@result.should == n.to_i
+    page.has_css?("answers")    
 end
-#https://www.wolframalpha.com/input/?i=80-70
