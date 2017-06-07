7330fcabeb3100c7834c1332f842bb7cb3744fea
Ale <alejandra_siles_a@hotmail.com>
2017-06-07 06:15:27 -0400

    Checking results

--- features/step_definitions/SubstractSteps.rb
+++ features/step_definitions/SubstractSteps.rb
@@ -9,5 +9,8 @@ When(/^I click (\w+)$/) do |compute|
 end
 
 Then /^The result should be (\d+) on the screen$/ do |n|
-    page.has_css?("answers")    
+    page.has_css?("answers")
+    xpath= '/html/body/div[1]/main/div/div[1]/wa-answers/section/section[2]/section/div[1]/div/img'
+    img = find(:xpath, xpath)
+    img['alt'].should == n
 end
--- features/step_definitions/cosine.rb
+++ features/step_definitions/cosine.rb
@@ -1,3 +1,3 @@
-Given(/^I have entered cos(\d+)\ into the textbox$/) do |arg1|
-fill_in 'query', :with => "cos("+arg1+")"
+Given(/^I have entered cos\((\d+)\) into the textbox$/) do |arg1|
+  fill_in 'query', :with => "cos("+arg1+")"
 end
